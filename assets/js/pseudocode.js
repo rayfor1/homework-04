939e7a81e0c7b3c20da98547ee80ef9ca380f432
github username <github email address>
2020-04-10 03:09:17 -0400

    initial push - setupup initial files for html, css, and js to be used for assignment

---
+++ assets/js/pseudocode.js
@@ -0,0 +1,27 @@
+//define our questions 
+
+//have an idea of the HTML layout
+
+//<h2 id=" "> will be a countDown timer (h2Tag in js)
+
+//<h2 id>
+//<ul> <li> will be for questions (ulTag and liTag)
+
+//create a START button with an eventListener attached to start the quiz
+
+//load in timer and Question 1
+
+//if and else statements to determine if question was correct or incorrect
+
+//indicate to user if they were correct or not
+
+//if incorrect: subtrct time (10 seconds)
+
+//if correct: increment score ( score ++)
+
+//Load next question 2 (repeat until step 5)
+
+// after final question is answered, load "GAME OVER" and indicate final score
+
+//use localStorage to save all info 
+    
---
+++ assets/js/question.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        question : "What does HTML stand for?",
+        option1 : "Correct",
+        option2 : "Wrong",
+        option3 : "Wrong",
+        option4 : "Wrong",
+        correct : "A"
+    },
+    
+    {
+        question : "What does CSS stand for?",
+        option1 : "Wrong",
+        option2 : "Correct",
+        option3 : "Wrong",
+        option4 : "Wrong",
+        correct : "B"
+    },
+    
+    {
+        question : "What does JS stand for?",
+        option1 : "Wrong",
+        option2 : "Wrong",
+        option3 : "Correct",
+        option4 : "Wrong",
+        correct : "C"
+    }
+];
